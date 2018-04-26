3564785e88b0b44b0b1df6df88209203898adce7
Admir Tiric <Admirtiric25@Hotmail.com>
2018-04-26 03:11:21 -0700

    '1.6'

--- config/db.js
+++ config/db.js
@@ -1,5 +1,6 @@
 module.exports = {
-	url : 'mongodb://localhost/meanshop-dev',
+	// url : 'mongodb://localhost/meanshop-dev',
+	url: 'mongodb://root:password@ds259109.mlab.com:59109/nexcommerce',
 
 	seedDB: true
 }
